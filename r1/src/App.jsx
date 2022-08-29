7a7fac05cc3be10a050a57db6c6fa2b94def0a30
margari25 <budryte.m@mail.ru>
2022-08-29 22:02:52 +0300

    update with functions

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>STATE 2</h1>
                 <Squares />
             </header>
         </div>
--- r1/src/Components/006/Squares.jsx
+++ r1/src/Components/006/Squares.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import rand from "../../Functions/rand";
+import randomColor from "../../Functions/randomColor";
 
 function Squares() {
 
     const [sq, setSq] = useState([]);
 
     const add = () => {
-        setSq(s => [...s, '']);
+        setSq(s => [...s, { number: rand(100, 999), color: randomColor() }]);
     }
+
+    const sortDesc = () => {
+        setSq(s => [...s].sort((a, b) => b.number - a.number));
+    }
+
 
     return (
         <>
+            <h1>STATE 2 {sq.filter(s => s.number < 300).length}</h1>
             <div className="container">
                 {
-                    sq.map((_, i) => <div key={i}>{i + 1}</div>)}
+                    sq.map((n, i) => <div style={{
+                        backgroundColor: n.number < 300 ? 'black' : n.color,
+                        borderRadius: n.number % 2 ? '0' : '50%',
+                    }} key={i}>{n.number}
+                    </div>)
+                }
             </div>
-            <button onClick={add}>add []</button>
+            <button onClick={add}>add square</button>
+            <button onClick={sortDesc}>Sort</button>
         </>
     )
 
