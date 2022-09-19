0603c2c7b4f27af855c11e4533853c9d91fa10ce
margari25 <budryte.m@mail.ru>
2022-09-19 19:59:51 +0300

    intervals

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,12 +1,38 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.scss';
-import Books from './Components/019/Books';
 
 function App() {
 
+    const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter(c => c + 1);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter2(c => c >= 10 ? 0 : c + 2);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+
+    
+           
     return (
         <div className="App">
             <header className="App-header">
-            <Books />
+            <h2>{counter}</h2>
+            <h2>{counter2}</h2>
             </header>
         </div>
     );
